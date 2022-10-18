8a59cb8b73a86427d95648bd5b656ecbe7fc5fa2
Shakir <beefthesavage@protonmail.com>
2022-10-18 11:55:54 +0100

    queries the api multiple times

--- generate_data.py
+++ generate_data.py
@@ -1,27 +1,30 @@
 import requests
+import random
+import datetime 
+import time
 
-API_URL = "https://www.googleapis.com/books/v1/volumes?q=cars"
 book_list = []
+book_count = 1
 
 
 def get_book_data(url):
     return requests.get(url).json()
 
 
-def parse_item(item, book_count) -> str:
-    if "categories" in item["volumeInfo"] and "retailPrice" in item["saleInfo"]:
+def parse_item(item) -> str:
+    if all(x in item["volumeInfo"] for x in ['categories', 'authors']) and "retailPrice" in item["saleInfo"]:
         return (
             f"{book_count},{item['volumeInfo']['categories'][0]},"
             f"{item['volumeInfo']['title']},{item['volumeInfo']['authors'][0]},"
-            f"${item['saleInfo']['retailPrice']['amount']}"
+            f"${item['saleInfo']['retailPrice']['amount']},{random_date()}"
         )
     return None
 
 
 def parse_books(books):
-    book_count = 1
+    global book_count
     for book in books["items"]:
-        book_string = parse_item(book, book_count)
+        book_string = parse_item(book)
         if book_string is not None:
             book_list.append(book_string)
             book_count += 1
@@ -29,14 +32,23 @@ def parse_books(books):
 
 def save_books(filename):
     with open(filename, "w", encoding="UTF-8") as file_:
-        for line in file_:
-            file_.write("\n".join(line))
+        file_.write("\n".join(book_list))
 
+def random_date():
+    start = datetime.date(2019, 2, 1)
+    time_between_dates = datetime.date(2022, 9, 1) - datetime.date(2019, 2, 1)
+    random_number_of_days = random.randrange(time_between_dates.days)
+    return start + datetime.timedelta(days=random_number_of_days)
 
 def main():
-    book_data = get_book_data(API_URL)
-    parse_books(book_data)
+    keywords = ['harry', 'eagles', 'boats', 'cars', 
+    'people', 'sea', 'ocean', 'england', 'building', 'houses', 'london',
+    'note', 'grapeboy', 'connection', 'eletric']
+    for k in keywords:
+        #time.sleep(1)
+        book_data = get_book_data(f"https://www.googleapis.com/books/v1/volumes?q={k}&maxResults=40")
+        parse_books(book_data)
+    save_books('data.txt')
     print(book_list)
-
 
 main()
