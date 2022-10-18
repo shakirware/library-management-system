4b141533c2e493279edbc0adc8668ae05ae36d6e
Shakir <beefthesavage@protonmail.com>
2022-10-18 12:38:14 +0100

    updated and added loan generation

--- generate_data.py
+++ generate_data.py
@@ -1,25 +1,46 @@
-import requests
 import random
-import datetime 
+import datetime
 import time
+import requests
 
 book_list = []
+loan_list = []
 book_count = 1
 
+keywords = [
+        "harry",
+        "eagles",
+        "boats",
+        "cars",
+        "people",
+        "sea",
+        "ocean",
+        "england",
+        "building",
+        "houses",
+        "london",
+        "note",
+        "grapeboy",
+        "connection",
+        "eletric",
+        "maths",
+        "physics",
+    ]
 
 def get_book_data(url):
     return requests.get(url).json()
 
-
 def parse_item(item) -> str:
-    if all(x in item["volumeInfo"] for x in ['categories', 'authors']) and "retailPrice" in item["saleInfo"]:
+    if (
+        all(x in item["volumeInfo"] for x in ["categories", "authors"])
+        and "retailPrice" in item["saleInfo"]
+    ):
         return (
             f"{book_count},{item['volumeInfo']['categories'][0]},"
             f"{item['volumeInfo']['title']},{item['volumeInfo']['authors'][0]},"
             f"${item['saleInfo']['retailPrice']['amount']},{random_date()}"
         )
     return None
-
 
 def parse_books(books):
     global book_count
@@ -28,27 +49,36 @@ def parse_books(books):
         if book_string is not None:
             book_list.append(book_string)
             book_count += 1
-
-
-def save_books(filename):
+        
+def save_file(filename, array):
     with open(filename, "w", encoding="UTF-8") as file_:
-        file_.write("\n".join(book_list))
+        file_.write("\n".join(array))
 
 def random_date():
     start = datetime.date(2019, 2, 1)
-    time_between_dates = datetime.date(2022, 9, 1) - datetime.date(2019, 2, 1)
+    time_between_dates = datetime.date(2022, 9, 1) - start
     random_number_of_days = random.randrange(time_between_dates.days)
     return start + datetime.timedelta(days=random_number_of_days)
 
-def main():
-    keywords = ['harry', 'eagles', 'boats', 'cars', 
-    'people', 'sea', 'ocean', 'england', 'building', 'houses', 'london',
-    'note', 'grapeboy', 'connection', 'eletric']
-    for k in keywords:
-        #time.sleep(1)
-        book_data = get_book_data(f"https://www.googleapis.com/books/v1/volumes?q={k}&maxResults=40")
+def loan_string():
+    return f'{random.randint(1,len(book_list))},{random_date()},{random_date()},{random.randint(1111,2222)}'
+
+def generate_books(keywords):
+    for keyword in keywords:
+        book_data = get_book_data(
+            f"https://www.googleapis.com/books/v1/volumes?q={keyword}&maxResults=40"
+        )
         parse_books(book_data)
-    save_books('data.txt')
-    print(book_list)
+        save_file("Book_Info.txt", book_list)
+
+def generate_loans(amount):
+    for i in range(1, amount):
+        loan_list.append(loan_string())
+    save_file("Loan_Reservation_History.txt", loan_list)
+    
+
+def main():
+    generate_books(keywords)
+    generate_loans(200)
 
 main()
